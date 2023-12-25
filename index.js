5fd398da5f3ea5acd43a668121daae701495bd42
khagendra01 <khagendrakhatri365@gmail.com>
2023-12-25 10:21:58 -0500

    balancer

---
+++ index.js
@@ -0,0 +1,8 @@
+function calculateBMI(weight, height) {
+  return weight / height ** 2;
+}   
+console.log(calculateBMI(80, 1.8)); 
+
+function calculateAge(birthYear) {
+  return 2023 - birthYear;
+}
