c97eeded1c204b0816fd1f5b37e6acaa666d325a
kkhatri@caldwell.edu <khagendrakhatri365@gmail.com>
2023-12-24 20:31:16 -0500

    birth

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function calculateBMI(weight, height) {
   return weight / height ** 2;
 }   
 console.log(calculateBMI(80, 1.8)); 
+
+function calculateAge(birthYear) {
+  return 2023 - birthYear;
+}
