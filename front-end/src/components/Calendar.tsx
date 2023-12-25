aa61d19dd6b81903284d8a2800df9141e3dea298
kkhatri@caldwell.edu <khagendrakhatri365@gmail.com>
2023-12-25 12:29:19 -0500

    motr

--- front-end/src/components/Calendar.tsx
+++ front-end/src/components/Calendar.tsx
@@ -1,5 +1,4 @@
 import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
-
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { FaRegCalendarAlt } from 'react-icons/fa';
@@ -24,15 +23,15 @@ const Calendar = () => {
 
     return (
         <>
-        <div className='flex flex-col md:flex-row justify-between items-center bg-gray-900 py-12 px-12 md:px-24 lg:px-32 xl:px-48 2xl:px-64'>
-            <div className='flex flex-col items-center text-left space-y-4 calendar-box mr-4'>
+        <div className='flex flex-col justify-between items-center bg-gray-900 py-12 px-4 md:px-12 lg:px-24 xl:px-32 2xl:px-48'>
+            <div className='flex flex-col items-center text-left space-y-4 calendar-box mb-4'>
                 <h1 className='text-4xl font-semibold text-black'> {month} </h1>
                 <h1 className='text-4xl font-semibold text-black'><FaRegCalendarAlt /></h1>
                 <h1 className='text-3xl font-semibold text-black'>{day}</h1>
             </div>
-            <textarea className='flex-grow h-full bg-gray-200 rounded-md px-4 py-2 mr-4' placeholder='Enter your text here...'></textarea>
-            <div className='flex items-center space-x-4'>
-                <button className='bg-primary text-white px-4 py-2 rounded-md mr-2'>Submit</button>
+            <textarea className='w-full h-full bg-gray-200 rounded-md px-4 py-2 mb-4' placeholder='Share your amazing story of the day...'></textarea>
+            <div className='flex justify-center items-center space-x-4'>
+                <button className='bg-primary text-white px-4 py-2 rounded-md'>Submit</button>
                 <button className='bg-primary text-white px-4 py-2 rounded-md'>Edit</button>
             </div>
         </div>
@@ -44,6 +43,6 @@ const Calendar = () => {
         />
     </>
     );
-    };
-    
-    export default Calendar;
+};
+
+export default Calendar;
