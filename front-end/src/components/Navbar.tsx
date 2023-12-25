58411322973d9c9730db6ce6d11a8f5415137199
kkhatri@caldwell.edu <khagendrakhatri365@gmail.com>
2023-12-24 23:48:58 -0500

    finalfortoight

--- front-end/src/components/Navbar.tsx
+++ front-end/src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 
 
+import { useState } from 'react';
 import logo from '../assets/logo.png';
 
 const navItems = [
@@ -9,16 +10,24 @@ const navItems = [
     { link: "About", path: "about" },
 ]
 
-function Navbar() {
-    return (
-        <nav className='bg-gray-900 text-white py-4 px-12 md:px-24 lg:px-32 xl:px-48 2xl:px-64'> {/* 2xl:px-64 is for 2xl screens and above */}
-            <div>
-                <div>
-                    <a href="/" className='text-2xl font-semibold flex items-center space-x-3 text-primary'>
-                        <img src={logo} alt="logo" /> <span>XYZ</span>
-                    </a>
+const Navbar = () => {
 
-                    <ul className='md:flex space-x-12'>
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+    return (
+        <>
+            <nav className='bg-gray-900 text-white py-4 px-12 md:px-24 lg:px-32 xl:px-48 2xl:px-64 fixed top-0 right-0 left-0'>
+                <div className='flex justify-between items-center'>
+                    <div className='flex items-center space-x-3'>
+                        <a href="/" className='text-2xl font-semibold flex items-center space-x-3 text-primary'>
+                            <img src={logo} alt="logo" style={{ width: "50px", height: "50px" }} /><span>XYZ</span>
+                        </a>
+                    </div>
+                    <ul className='hidden md:flex space-x-12 mx-auto'>
                         {navItems.map((item, index) => (
                             <li key={index}>
                                 <a href={`#${item.path}`} className='block hover:text-gray-300'>{item.link}</a>
@@ -34,9 +43,30 @@ function Navbar() {
                             <option>Spanish</option>
                         </select>
                     </div>
+
+                    {/* mobile menu */}
+                    <div className='md:hidden flex items-center'>
+                        <button className='mobile-menu-button' onClick={toggleMenu}>
+                            {isMenuOpen ? (
+                                <svg className='w-6 h-6 text-white' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                                    <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M6 18L18 6M6 6l12 12'></path>
+                                </svg>
+                            ) : (
+                                <svg className='w-6 h-6 text-white' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                                    <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16M4 18h16'></path>
+                                </svg>
+                            )}
+                        </button>
+                    </div>
                 </div>
+            </nav>
+
+            {/* mobile menu */}
+            <div className={`space-y-4 px-4 pt-24 pb-5 bg-secondary transition-all duration-500 ease-in-out overflow-hidden ${isMenuOpen ? 'opacity-100 h-auto' : 'opacity-0 h-0'}`}>                {navItems.map((item, index) => (
+                    <a href={`#${item.path}`} className='block hover:text-gray-300'>{item.link}</a>
+                ))}
             </div>
-        </nav>
+        </>
     );
 }
 
