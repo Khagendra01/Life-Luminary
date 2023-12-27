1a2e8da4a2a16b289a5442e53e94b178a05b7d51
khagendra01 <khagendrakhatri365@gmail.com>
2023-12-27 09:11:38 -0500

    feww feature

---
+++ front-end/src/api/instance.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const instance = axios.create({ baseURL: `https://localhost:7120` });
+
+instance.interceptors.request.use(config => {
+  const token = localStorage.getItem("accessToken");
+  
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
+
+instance.interceptors.response.use(
+  response => {
+    if (response.data && response.data.isSuccess) {
+      return Promise.resolve(response.data.result);
+    } else {
+      return Promise.reject(response.data.message || 'Request failed');
+    }
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
