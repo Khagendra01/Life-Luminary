1311269f900c5c5333840a2d193a0883365a370e
kkhatri@caldwell.edu <khagendrakhatri365@gmail.com>
2023-12-28 14:35:35 -0500

    trnsfer back

--- front-end/src/routes/Routes.tsx
+++ front-end/src/routes/Routes.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { useContext } from "react";
 
 import About from "../pages/About";
 import Home from "../pages/Home";
@@ -8,23 +9,22 @@ import BedTime from "../pages/BedTime";
 import Register from "../components/Register";
 import Login from "../components/Login";
 import Activity from "../pages/Activity";
-import { useContext } from "react";
 import { AuthContext } from "../App";
 
 const RouteConfig = () => {
+  const { user } = useContext(AuthContext) || {};
 
-  const { user } = useContext(AuthContext);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/activity" element={<Activity />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/feed" element={<Feed />} />
-        <Route path="/bedtime" element={<BedTime />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/activity" element={user ? <Activity /> : <Navigate to="/login" />} />
+        <Route path="/bedtime" element={user ? <BedTime /> : <Navigate to="/login" />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </BrowserRouter>
