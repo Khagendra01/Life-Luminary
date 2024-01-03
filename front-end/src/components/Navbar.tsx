5ae3443c09209e87cf394359e09697ec7b6dd13e
kkhatri@caldwell.edu <khagendrakhatri365@gmail.com>
2024-01-03 09:59:01 -0500

    up by mac

--- front-end/src/components/Navbar.tsx
+++ front-end/src/components/Navbar.tsx
@@ -3,6 +3,9 @@
 import { useContext, useState } from 'react';
 import logo from '../assets/logo.png';
 
+import { FaUser } from 'react-icons/fa';
+
+
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../App';
 
@@ -10,7 +13,9 @@ const Navbar = () => {
 
     //const { user } = useContext(AuthContext) || {};
 
-    const user = false;
+    const [dropdownVisible, setDropdownVisible] = useState(false);
+
+    const user = true;
 
     const navItems = user ? [
         { link: "Feed", path: "Feed" },
@@ -32,16 +37,31 @@ const Navbar = () => {
 
     const navigate = useNavigate();
 
-    //const user = true
-
     return (
         <>
-            <nav className='bg-gradient-to-r from-primary to-secondary  text-white py-4 px-2 md:px-8 lg:px-16 xl:px-24 2xl:px-64 fixed top-0 right-0 left-0'>
+            <nav className='bg-gradient-to-r from-primary to-secondary text-white py-4 px-4 md:px-8 lg:px-16 xl:px-24 2xl:px-64 fixed top-0 right-0 left-0'>
                 <div className='flex justify-between items-center'>
-                    <div className='flex items-center space-x-3 '>
-                        <a href="/" className='text-2xl font-semibold flex items-center space-x-3 text-white'>
-                            <img src={logo} alt="logo" style={{ width: "50px", height: "50px" }} /><span className='md:block hidden'>Life Luminary</span>
-                        </a>
+                    {/* mobile menu */}
+                    <div className='flex px-4 justify-between'>
+                        <div className='md:hidden flex items-center justify-end mr-4'>
+                            <button className='mobile-menu-button' onClick={toggleMenu}>
+                                {isMenuOpen ? (
+                                    <svg className='w-6 h-6 text-white' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M6 18L18 6M6 6l12 12'></path>
+                                    </svg>
+                                ) : (
+                                    <svg className='w-6 h-6 text-white' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16M4 18h16'></path>
+                                    </svg>
+                                )}
+                            </button>
+                        </div>
+                        {/* logo */}
+                        <div className='flex items-center space-x-3 '>
+                            <a href="/" className='text-2xl font-semibold flex items-center space-x-3 text-white'>
+                                <img src={logo} alt="logo" style={{ width: "50px", height: "50px" }} /><span className='md:block hidden'>Life Luminary</span>
+                            </a>
+                        </div>
                     </div>
                     <div>
                         <ul className='hidden md:flex space-x-12 mx-auto'>
@@ -54,49 +74,81 @@ const Navbar = () => {
                     </div>
 
                     <div className='hidden md:flex items-center space-x-4'>
-                        <button
-                            onClick={() => navigate('/register')}
-                            className='bg-primary text-white w-24 h-10 rounded-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r hover:from-green-400 hover:to-blue-500'
-                        >
-                            Sign Up
-                        </button>
-                        <button
-                            onClick={() => navigate('/login')}
-                            className='text-white w-24 h-10 px-4 py-2 rounded-md border border-white transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110'
-                        >
-                            Sign In
-                        </button>
+                        {user ? (
+                            <div className="relative inline-block text-left" onClick={() => setDropdownVisible(!dropdownVisible)}>
+                                <div>
+                                    <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+                                        <FaUser />
+                                    </button>
+                                </div>
+
+                                <div className={`origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 ${dropdownVisible ? '' : 'hidden'}`}>
+                                    <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                                        <button onClick={() => navigate('/profile')} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Profile</button>
+                                        <button onClick={() => navigate('/settings')} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Settings</button>
+                                        <button onClick={() => { }} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Logout</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        ) : (
+                            <>
+                                <button
+                                    onClick={() => navigate('/register')}
+                                    className='bg-primary text-white w-24 h-10 rounded-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r hover:from-green-400 hover:to-blue-500'
+                                >
+                                    Sign Up
+                                </button>
+                                <button
+                                    onClick={() => navigate('/login')}
+                                    className='text-white w-24 h-10 px-4 py-2 rounded-md border border-white transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110'
+                                >
+                                    Sign In
+                                </button>
+                            </>
+                        )}
                     </div>
 
 
                     {/* mobile menu */}
                     <div className='md:hidden flex items-center justify-end'>
-                        <button
-                            onClick={() => navigate('/register')}
-                            className='bg-primary text-white w-24 h-10 rounded-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r hover:from-green-400 hover:to-blue-500'
-                        >
-                            Sign Up
-                        </button>
-                        <button className='mobile-menu-button' onClick={toggleMenu}>
-                            {isMenuOpen ? (
-                                <svg className='w-6 h-6 text-white' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-                                    <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M6 18L18 6M6 6l12 12'></path>
-                                </svg>
-                            ) : (
-                                <svg className='w-6 h-6 text-white' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-                                    <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16M4 18h16'></path>
-                                </svg>
-                            )}
-                        </button>
+                        {user ? (
+                            <div className="relative inline-block text-left" onClick={() => setDropdownVisible(!dropdownVisible)}>
+                                <div>
+                                    <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+                                        <FaUser />
+                                    </button>
+                                </div>
+
+                                <div className={`origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 ${dropdownVisible ? '' : 'hidden'}`}>
+                                    <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                                        <button onClick={() => navigate('/profile')} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Profile</button>
+                                        <button onClick={() => navigate('/settings')} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Settings</button>
+                                        <button onClick={() => { }} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Logout</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        ) : (
+                            <button
+                                onClick={() => navigate('/register')}
+                                className='bg-primary text-white w-24 h-10 rounded-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r hover:from-green-400 hover:to-blue-500'
+                            >
+                                Sign Up
+                            </button>)}
                     </div>
                 </div>
             </nav>
 
             {/* mobile menu */}
-            <div className={`space-y-4 px-4 pt-24 pb-5 bg-secondary transition-all duration-500 ease-in-out overflow-hidden ${isMenuOpen ? 'opacity-100 h-auto' : 'opacity-0 h-0'}`}>                {navItems.map((item) => (
-                <a href={`${item.path}`} className='block hover:text-gray-300'>{item.link}</a>
-            ))}
-            </div>
+            <div className={`space-y-4 px-4 pt-24 pb-5 bg-gradient-to-r from-primary to-secondary transition-all duration-500 ease-in-out overflow-hidden ${isMenuOpen ? 'opacity-100 h-auto' : 'opacity-0 h-0'}`}>
+    {navItems.map((item, index) => (
+        <div key={index}>
+            <a href={`${item.path}`} className='block text-sm font-medium text-white hover:text-gray-300 transition-colors duration-200'>{item.link}</a>
+            <hr />
+        </div>
+    ))}
+</div>
 
 
 
