ad34e2c2c4ef906b0c67a38fef79c9ff21ee9d0c
khagendra01 <khagendrakhatri365@gmail.com>
2024-01-05 08:58:14 -0500

    push

--- front-end/src/components/Login.tsx
+++ front-end/src/components/Login.tsx
@@ -6,6 +6,17 @@ import { useContext, useState } from 'react';
 import { login } from '../api/authApi';
 import { AuthContext } from '../App';
 
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Input,
+    Checkbox,
+    Button,
+  } from "@material-tailwind/react";
+
 const Login = () => {
 
     const [loginInfo, setloginInfo] = useState<LoginInfo>({ userName: "", password: ""});
@@ -52,6 +63,43 @@ const Login = () => {
     return (
         <>
         <Navbar />
+        <Card className="w-96">
+      <CardHeader
+        variant="gradient"
+        color="gray"
+        className="mb-4 grid h-28 place-items-center"
+      >
+        <Typography variant="h3" color="white">
+          Sign In
+        </Typography>
+      </CardHeader>
+      <CardBody className="flex flex-col gap-4">
+        <Input label="Email" size="lg" />
+        <Input label="Password" size="lg" />
+        <div className="-ml-2.5">
+          <Checkbox label="Remember Me" />
+        </div>
+      </CardBody>
+      <CardFooter className="pt-0">
+        <Button variant="gradient" fullWidth>
+          Sign In
+        </Button>
+        <Typography variant="small" className="mt-6 flex justify-center">
+          Don&apos;t have an account?
+          <Typography
+            as="a"
+            href="#signup"
+            variant="small"
+            color="blue-gray"
+            className="ml-1 font-bold"
+          >
+            Sign up
+          </Typography>
+        </Typography>
+      </CardFooter>
+    </Card>
+
+
         <div className="bg-cover bg-center z-50 flex items-center justify-center mt-16">
             <div className="bg-white p-8 rounded-lg shadow-2xl max-w-sm mx-auto">
                 <h2 className="text-3xl font-bold mb-6 text-center">Login</h2>
